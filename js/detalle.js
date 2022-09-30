3072f9b589105ac01e8f50542b97dd861c70be2e
Iván <ivandivi21@gmail.com>
2022-09-30 00:43:28 -0300

    a

--- js/detalle.js
+++ js/detalle.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     idConseguido = localStorage.getItem("MonedaElegida");
     var url = new URL("https://api.coingecko.com/api/v3/coins/" + idConseguido);
     loadIntoDetalle(url);
@@ -8,17 +8,12 @@ window.addEventListener("load", function() {
 async function loadIntoDetalle(url) {
     const descripcion = document.getElementById("descripcionCrypto");
     const nombreDetalle = document.getElementById("nombreCrypto");
+    const precioDetalle = document.getElementById("precioDetalle")
     const response = await fetch(url);
     const data = await response.json();
     monedaDetalle = data
     nombreDetalle.innerHTML = monedaDetalle.id
     descripcion.innerHTML = monedaDetalle.description.en
+    precioDetalle.innerHTML = monedaDetalle.market_data.current_price.usd
     console.log(data);
 }
-//monedaDetalle.description.en
-
-
-
-
-
-
