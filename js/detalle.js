f0d632efd0f0ac0d0e7d52cc6ac22508dd83c087
Iván <ivandivi21@gmail.com>
2022-10-14 08:35:03 -0300

    Metiendo datos de api que faltaban

--- js/detalle.js
+++ js/detalle.js
@@ -8,12 +8,18 @@ window.addEventListener("load", function () {
 async function loadIntoDetalle(url) {
     const descripcion = document.getElementById("descripcionCrypto");
     const nombreDetalle = document.getElementById("nombreCrypto");
-    const precioDetalle = document.getElementById("precioDetalle")
+    const precioDetalle = document.getElementById("precioCrypto");
+    const SiglaDetalle = document.getElementById("siglaCrypto");
+    const porcentajeDetalle = document.getElementById("porcentajeCrypto")
+    const SiglaDetalle2 = document.getElementById("siglaCrypto2");
     const response = await fetch(url);
     const data = await response.json();
     monedaDetalle = data
-    nombreDetalle.innerHTML = monedaDetalle.id
+    nombreDetalle.innerHTML = (monedaDetalle).id
     descripcion.innerHTML = monedaDetalle.description.en
     precioDetalle.innerHTML = monedaDetalle.market_data.current_price.usd
+    SiglaDetalle.innerHTML = monedaDetalle.symbol
+    SiglaDetalle2.innerHTML = monedaDetalle.symbol
+    porcentajeDetalle.innerHTML = "Last 7 Days " + monedaDetalle.market_data.price_change_percentage_7d.toFixed(2) + "%"
     console.log(data);
 }
